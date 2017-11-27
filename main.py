746359ba41119ad2b766e726a699102c13f277c0
JustineNavaja <justine.navaja98@gmail.com>
2017-11-27 13:01:06 +0800

    K-means algorithm evaluation and file writing

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-from . import extract_values_file, extract_values_image, get_sample, modify_rgba
+from utils import extract_values_file, extract_values_image, get_sample, modify_rgba, get_assignments, get_new_centroids, get_cost_j
 
+class Main(object):
+	def __init__(self):
+		super(Main, self).__init__()
+		
+	def solve_k_means(self, values, centroids):
+		J = 0
+		for iter in xrange(1,11):
+			assignments = get_assignments(values, centroids)
+
+			centroids = get_new_centroids(values, assignments, len(centroids))
+			# print("\n")
+			# print(new_centroids)
+
+			new_j = get_cost_j(assignments, len(values))
+			# print("\n")
+			# print new_j
+
+			dJ = J - new_j
+			self.write_new_assignments(assignments, iter)
+			self.write_new_centroids(centroids, new_j, dJ, iter)
+			J = new_j
+		print("First part accomplished!")
+
+	def write_new_assignments(self, assignments, iter):
+	    file_name = "iter" + str(iter) + "_ca.txt"
+	    file = open(file_name, "w")
+	    for value in assignments:
+	        file.write("" + str(value[1] + 1) + "\n")
+	    file.close()
+
+	def write_new_centroids(self, centroids, J, dJ, iter):
+		file_name = "iter" + str(iter) + "_cm.txt"
+		file = open(file_name, "w")
+		for value in centroids:
+			file.write("" + str(value[0]) + " " + str(value[1]) + "\n")
+		file.write("J= " + str(J) + "\n")
+		file.write("dJ= " + str(dJ) + "\n")
+		file.close()
 
 values_from_file = extract_values_file(filename="kmdata1.txt")
 file_centroids = [
@@ -7,12 +45,22 @@ file_centroids = [
     [6, 2],
     [8, 5]
 ]
-print(values_from_file)
 
 values_from_image = extract_values_image(filename="kmimg1.png")
 image_centroids = get_sample(values=values_from_image, num=16)
 
+for values in image_centroids:
+	print len(values)
+	print "\n"
+print(len(image_centroids))
+# print(values_from_image)
+# print("\n\n\n\n\n\n")
+# print(image_centroids)
 
-new_values = values_from_image
-modify_rgba(source="kmimg1.png", target="kmimg2.png", values=new_values)
+main = Main()
+main.solve_k_means(values_from_file, file_centroids)
+
+# new_values = 
+# modify_rgba(source="kmimg1.png", target="kmimg2.png", values=new_values)
+
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import random
+import math
 from PIL import Image
 
 def extract_values_file(filename=None):
@@ -45,3 +46,57 @@ def modify_rgba(source=None, output=None, values=[]):
                 pix[ridx, cidx] = tuple(values[ridx][cidx])
 
         im.save(output)
+
+def get_assignments(values, centroids):
+    assignments = []
+
+    for pair in values:
+        distances = []
+
+        for centroid in centroids:
+            distance = get_distance(pair, centroid)
+            distances.append(distance)
+
+        min_distance = min(distances)
+        assignments.append([math.ceil(min_distance*1000000)/1000000, distances.index(min_distance)])
+        #assignments.append(centroids[get_min_centroid(a, b, c)]);
+    return assignments
+
+def get_distance(pair, centroid):
+    return math.sqrt((pair[0] - centroid[0])**(2) + (pair[1] - centroid[1])**(2))
+
+def get_min_centroid(a, b ,c):
+    min = a
+    index = 0
+    if min > b:
+        min = b
+        index = 1
+    if min > c:
+        min = c
+        index = 2
+    return index
+
+def get_new_centroids(values, assignments, length):
+    new_centroids = []
+    mu_sizes = []
+    while length > 0:
+        new_centroids.append([0, 0])
+        mu_sizes.append(0)
+        length -= 1;
+
+    for index, pair in enumerate(values):
+        i = assignments[index][1]
+        new_centroids[i] = [(new_centroids[i][0] + values[index][0]), (new_centroids[i][1] + values[index][1])]
+        mu_sizes[i] += 1
+
+    for index, pair in enumerate(new_centroids):
+        new_centroids[index][0] /= mu_sizes[index]
+        new_centroids[index][1] /= mu_sizes[index]
+
+        new_centroids[index][0] = math.ceil(new_centroids[index][0]*1000000)/1000000
+        new_centroids[index][1] = math.ceil(new_centroids[index][1]*1000000)/1000000
+    return new_centroids  
+
+def get_cost_j(assignments, m):
+    cost_j = (sum(x[0] for x in assignments))/m
+    return cost_j
